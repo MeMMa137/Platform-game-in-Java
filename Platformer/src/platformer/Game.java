9165ecf46038a5a7c22bfb8b51b15a0484491000
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-17 12:03:34 +0200

    Game class creation

--- Platformer/src/platformer/Game.java
+++ Platformer/src/platformer/Game.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package platformer;
 
-/**
- *
- * @author emmab
- */
-class Game {
+import java.awt.Graphics;
+import gamestates.Gamestate;
+import gamestates.Menu;
+import gamestates.Playing;
+
+public class Game implements Runnable {
+
+    private GameWindow gameWindow;
+    private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
+
+    private Playing playing;
+    private Menu menu;
+
+    public final static int TILES_DEFAULT_SIZE = 32;
+    public final static float SCALE = 1.75f;
+    public final static int TILES_IN_WIDTH = 26;
+    public final static int TILES_IN_HEIGHT = 14;
+    public final static int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
+    public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
+    public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
+
+    public Game() {
+        initClasses();
+        System.out.println("WIDTH: " + GAME_WIDTH + " HEIGHT: " + GAME_HEIGHT);
+
+        gamePanel = new GamePanel(this);
+        gameWindow = new GameWindow(gamePanel);
+        gamePanel.setFocusable(true);
+        gamePanel.requestFocus();
+
+        startGameLoop();
+
+    }
+
+    private void initClasses() {
+        menu = new Menu(this);
+        playing = new Playing(this);
+    }
+
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void update() {
+        switch (Gamestate.state) {
+            case MENU:
+                menu.update();
+                break;
+            case PLAYING:
+                playing.update();
+                break;
+            case OPTIONS:
+            case QUIT:
+            default:
+                System.exit(0);
+                break;
+
+        }
+    }
+
     
-}
