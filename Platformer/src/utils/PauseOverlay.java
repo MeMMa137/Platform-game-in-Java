c983bbe6df49061e0c6f8132a5cb349cb833e09d
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-19 12:05:24 +0200

    Additional buttons

---
+++ Platformer/src/utils/PauseOverlay.java
@@ -0,0 +1,59 @@
+package ui;
+
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+import gamestates.Gamestate;
+import gamestates.Playing;
+import platformer.Game;
+import utils.Constants;
+import utils.LoadSave;
+import static utils.Constants.UI.PauseButtons.*;
+import static utils.Constants.UI.URMButtons.*;
+import static utils.Constants.UI.VolumeButtons.*;
+
+public class PauseOverlay {
+
+    private Playing playing;
+    private BufferedImage backgroundImg;
+    private int bgX, bgY, bgW, bgH;
+    private SoundButton musicButton, sfxButton;
+    private UrmButton menuB, replayB, unpauseB;
+    private VolumeButton volumeButton;
+
+    public PauseOverlay(Playing playing) {
+        this.playing = playing;
+        loadBackground();
+        createSoundButtons();
+        createUrmButtons();
+        createVolumeButton();
+
+    }
+
+    private void createVolumeButton() {
+        int vX = (int) (309 * Game.SCALE);
+        int vY = (int) (278 * Game.SCALE);
+        volumeButton = new VolumeButton(vX, vY, SLIDER_WIDTH, VOLUME_HEIGHT);
+    }
+
+    private void createUrmButtons() {
+        int menuX = (int) (313 * Game.SCALE);
+        int replayX = (int) (387 * Game.SCALE);
+        int unpauseX = (int) (462 * Game.SCALE);
+        int bY = (int) (325 * Game.SCALE);
+
+        menuB = new UrmButton(menuX, bY, URM_SIZE, URM_SIZE, 2);
+        replayB = new UrmButton(replayX, bY, URM_SIZE, URM_SIZE, 1);
+        unpauseB = new UrmButton(unpauseX, bY, URM_SIZE, URM_SIZE, 0);
+
+    }
+
+    private void createSoundButtons() {
+        int soundX = (int) (450 * Game.SCALE);
+        int musicY = (int) (140 * Game.SCALE);
+        int sfxY = (int) (186 * Game.SCALE);
+        musicButton = new SoundButton(soundX, musicY, SOUND_SIZE, SOUND_SIZE);
+        sfxButton = new SoundButton(soundX, sfxY, SOUND_SIZE, SOUND_SIZE);
+
+    }
