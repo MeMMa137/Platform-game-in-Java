2601a952902011a4abbcf80a06344021d4dcacf6
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-19 16:11:50 +0200

    Import graphics and enemy initiates

---
+++ Platformer/src/entities/Crabby.java
@@ -0,0 +1,76 @@
+package entities;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
+import platformer.Game;
+import static utils.Constants.Directions.*;
+import static utils.Constants.EnemyConstants.*;
+import static utils.HelpMethods.*;
+
+/**
+ *
+ * @author Angel Sánchez (Atkar)
+ */
+public class Crabby extends Enemy {
+
+    //Attack box
+    private Rectangle2D.Float attackBox;
+    private int attackBoxOffsetX;
+
+    public Crabby(float x, float y) {
+        super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
+        initHitbox(x, y, (int) (22 * Game.SCALE), (int) (19 * Game.SCALE));
+        initAttackBox();
+    }
+
+    private void initAttackBox() {
+        attackBox = new Rectangle2D.Float(x, y, (int) (82 * Game.SCALE), (int) (19 * Game.SCALE));
+        attackBoxOffsetX = (int) (30 * Game.SCALE);
+    }
+
+    public void update(int[][] lvlData, Player player) {
+        updateBehavior(lvlData, player);
+        updateAnimationTick();
+        updateAttackBox();
+    }
+
+    private void updateAttackBox() {
+        attackBox.x = hitbox.x - attackBoxOffsetX;
+        attackBox.y = hitbox.y;
+    }
+
+    public void updateBehavior(int[][] lvlData, Player player) {
+        if (firstUpdate) {
+            firstUpdateCheck(lvlData);
+        }
+        if (inAir) {
+            updateInAir(lvlData);
+        } else {
+            switch (enemyState) {
+                case IDLE:
+                    newState(RUNNING);
+                    break;
+                case RUNNING:
+                    if (canSeePlayer(lvlData, player)) {
+                        turnTowardsPlayer(player);
+                    }
+                    if (isPlayerCloseForAttack(player)) {
+                        newState(ATTACK);
+                    }
+                    move(lvlData);
+                    break;
+                case ATTACK:
+                    if (aniIndex == 0) {
+                        attackChecked = false;
+                    }
+
+                    if (aniIndex == 3 && !attackChecked) {
+                        checkEnemyHit(attackBox, player);
+                    }
+                    break;
+                case HIT:
+                    break;
+            }
+        }
+    }
