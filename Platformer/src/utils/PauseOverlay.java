2bb35f17efc5d551ffa742e0549efa6b779bd1cc
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-19 13:04:04 +0200

    Pause screen

--- Platformer/src/utils/PauseOverlay.java
+++ Platformer/src/utils/PauseOverlay.java
@@ -57,3 +57,90 @@ public class PauseOverlay {
         sfxButton = new SoundButton(soundX, sfxY, SOUND_SIZE, SOUND_SIZE);
 
     }
+    
+ private void loadBackground() {
+        backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.PAUSE_BACKGROUND);
+        bgW = (int) (backgroundImg.getWidth() * Game.SCALE);
+        bgH = (int) (backgroundImg.getHeight() * Game.SCALE);
+        bgX = Game.GAME_WIDTH / 2 - bgW / 2;
+        bgY = (int) (25 * Game.SCALE);
+
+    }
+
+    public void update() {
+
+        musicButton.update();
+        sfxButton.update();
+
+        menuB.update();
+        replayB.update();
+        unpauseB.update();
+
+        volumeButton.update();
+
+    }
+
+    public void draw(Graphics g) {
+        // Background
+        g.drawImage(backgroundImg, bgX, bgY, bgW, bgH, null);
+
+        // Sound buttons
+        musicButton.draw(g);
+        sfxButton.draw(g);
+
+        // UrmButtons
+        menuB.draw(g);
+        replayB.draw(g);
+        unpauseB.draw(g);
+
+        // Volume Button
+        volumeButton.draw(g);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        if (volumeButton.isMousePressed()) {
+            volumeButton.changeX(e.getX());
+        }
+
+    }
+
+    public void mousePressed(MouseEvent e) {
+        if (isIn(e, musicButton)) {
+            musicButton.setMousePressed(true);
+        } else if (isIn(e, sfxButton)) {
+            sfxButton.setMousePressed(true);
+        } else if (isIn(e, menuB)) {
+            menuB.setMousePressed(true);
+        } else if (isIn(e, replayB)) {
+            replayB.setMousePressed(true);
+        } else if (isIn(e, unpauseB)) {
+            unpauseB.setMousePressed(true);
+        } else if (isIn(e, volumeButton)) {
+            volumeButton.setMousePressed(true);
+        }
+    }
+    public void mouseReleased(MouseEvent e) {
+        if (isIn(e, musicButton)) {
+            if (musicButton.isMousePressed()) {
+                musicButton.setMuted(!musicButton.isMuted());
+            }
+
+        } else if (isIn(e, sfxButton)) {
+            if (sfxButton.isMousePressed()) {
+                sfxButton.setMuted(!sfxButton.isMuted());
+            }
+        } else if (isIn(e, menuB)) {
+            if (menuB.isMousePressed()) {
+                Gamestate.state = Gamestate.MENU;
+                playing.unpauseGame();
+            }
+        } else if (isIn(e, replayB)) {
+            if (replayB.isMousePressed()) {
+                System.out.println("replay lvl!");
+            }
+        } else if (isIn(e, unpauseB)) {
+            if (unpauseB.isMousePressed()) {
+                playing.unpauseGame();
+            }
+        }
+
