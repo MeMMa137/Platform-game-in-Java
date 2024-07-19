b84876e94e0d0a7a44567eb8c1f4f57a3416fb52
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-19 11:05:09 +0200

    Finished options

--- Platformer/src/utils/HelpMethods.java
+++ Platformer/src/utils/HelpMethods.java
@@ -84,7 +84,28 @@ public class HelpMethods {
         return IsSolid(hitbox.x + xSpeed, hitbox.y + hitbox.height + 1, lvlData);
     }
 
-    
+    public static boolean IsAllTileWalkable(int xStart, int xEnd, int y, int[][] lvlData) {
+        for (int i = 0; i < xEnd - xStart; i++) {
+            if (IsTileSolid(xStart + i, y, lvlData)) {
+                return false;
+            }
+            if (!IsTileSolid(xStart + i, y + 1, lvlData)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean IsSightClear(int[][] lvlData, Rectangle2D.Float firstHitbox, Rectangle2D.Float secondHitbox, int yTile) {
+        int firstXTile = (int) (firstHitbox.x / Game.TILES_SIZE);
+        int secondXTile = (int) (secondHitbox.x / Game.TILES_SIZE);
+
+        if (firstXTile > secondXTile) {
+            return IsAllTileWalkable(secondXTile, firstXTile, yTile, lvlData);
+        } else {
+            return IsAllTileWalkable(firstXTile, secondXTile, yTile, lvlData);
+        }
+    }
 
 }
 
