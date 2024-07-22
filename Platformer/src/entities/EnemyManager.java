935a95b968515a3a55f599f92ab1cae8783ee9ab
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-22 11:06:50 +0200

    Entity and Player classes

--- Platformer/src/entities/EnemyManager.java
+++ Platformer/src/entities/EnemyManager.java
@@ -59,4 +59,19 @@ public void checkEnemyHit(Rectangle2D.Float attackBox) {
             }
         }
     }
+private void loadEnemyImgs() {
+        crabbyArr = new BufferedImage[5][9];
+        BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.CRABBY_SPRITE);
+        for (int j = 0; j < crabbyArr.length; j++) {
+            for (int i = 0; i < crabbyArr[j].length; i++) {
+                crabbyArr[j][i] = temp.getSubimage(i * CRABBY_WIDTH_DEFAULT, j * CRABBY_HEIGHT_DEFAULT, CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
+            }
+        }
+    }
+
+    public void resetAllEnemies() {
+        for (Crabby c : crabbies) {
+            c.resetEnemy();
+        }    
+    }
 }
---
+++ Platformer/src/entities/Player.java
@@ -0,0 +1,58 @@
+package entities;
+
+import gamestates.Playing;
+import java.awt.Color;
+import static utils.Constants.PlayerConstants.*;
+import static utils.HelpMethods.*;
+import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+
+import platformer.Game;
+import utils.LoadSave;
+import static utils.LoadSave.*;
+
+
+public class Player extends Entity {
+
+    private BufferedImage[][] animations;
+    private int aniTick, aniIndex, aniSpeed = 25;
+    private int playerAction = IDLE;
+    private boolean moving = false, attacking = false;
+    private boolean left, up, right, down, jump;
+    private float playerSpeed = 1.0f * Game.SCALE;
+    private int[][] lvlData;
+    private float xDrawOffset = 21 * Game.SCALE;
+    private float yDrawOffset = 4 * Game.SCALE;
+
+    // Jumping / Gravity
+    private float airSpeed = 0f;
+    private float gravity = 0.04f * Game.SCALE;
+    private float jumpSpeed = -2.25f * Game.SCALE;
+    private float fallSpeedAfterCollision = 0.5f * Game.SCALE;
+    private boolean inAir = false;
+
+    //Status Bar UI
+    private BufferedImage statusBarImg;
+
+    private int statusBarWidth = (int) (192 * Game.SCALE);
+    private int statusBarHeight = (int) (58 * Game.SCALE);
+    private int statusBarX = (int) (10 * Game.SCALE);
+    private int statusBarY = (int) (10 * Game.SCALE);
+    private int healthBarWidth = (int) (150 * Game.SCALE);
+    private int healthBarHeight = (int) (4 * Game.SCALE);
+    private int healthBarXStart = (int) (34 * Game.SCALE);
+    private int healthBarYStart = (int) (14 * Game.SCALE);
+
+    private int maxHealth = 100;
+    private int currentHealth = maxHealth;
+    private int healthWidth = healthBarWidth;
+
+    //Attack box
+    private Rectangle2D.Float attackBox;
+
+    private int flipX = 0;
+    private int flipW = 1;
+
+    private boolean attackChecked;
+    private Playing playing;
