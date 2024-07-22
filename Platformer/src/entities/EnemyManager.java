1fa9909cf44ba53200aa5cd1faa978e15a051d73
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-22 10:09:16 +0200

    Class to control enemies

---
+++ Platformer/src/entities/EnemyManager.java
@@ -0,0 +1,62 @@
+package entities;
+
+import gamestates.Playing;
+import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import utils.LoadSave;
+import static utils.Constants.EnemyConstants.*;
+
+public class EnemyManager {
+
+    private Playing playing;
+    private BufferedImage[][] crabbyArr;
+    private ArrayList<Crabby> crabbies = new ArrayList<>();
+
+    public EnemyManager(Playing playing) {
+        this.playing = playing;
+        loadEnemyImgs();
+        addEnemies();
+    }
+
+    private void addEnemies() {
+        crabbies = LoadSave.GetCrabs();
+        System.out.println("Tamaño de los cangrejos: " + crabbies.size());
+    }
+
+    public void update(int[][] lvlData, Player player) {
+        for (Crabby c : crabbies) {
+            if (c.isActive()) {
+                c.update(lvlData, player);
+            }
+        }
+    }
+
+    public void draw(Graphics g, int xLvlOffset) {
+        drawCrabs(g, xLvlOffset);
+    }
+
+    private void drawCrabs(Graphics g, int xLvlOffset) {
+        for (Crabby c : crabbies) {
+            if (c.isActive()) {
+                g.drawImage(crabbyArr[c.getEnemyState()][c.getAniIndex()],
+                        (int) c.getHitbox().x - xLvlOffset - CRABBY_DRAWOFFSET_X + c.flipX(),
+                        (int) c.getHitbox().y - CRABBY_DRAWOFFSET_Y,
+                        CRABBY_WIDTH * c.flipW(), CRABBY_HEIGHT, null);
+                c.drawAttackBox(g, xLvlOffset);
+            }
+        }
+    }
+    
+public void checkEnemyHit(Rectangle2D.Float attackBox) {
+        for (Crabby c : crabbies) {
+            if (c.isActive()) {
+                if (attackBox.intersects(c.getHitbox())) {
+                    c.hurt(10);
+                    return;
+                }
+            }
+        }
+    }
+}
